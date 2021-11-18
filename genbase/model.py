7185d83e09790742c3b472268b470fbf63cb112c
Robeer, M.J. (Marcel) <m.j.robeer@uu.nl>
2021-11-18 12:51:16 +0000

    Update model.py

--- genbase/model.py
+++ genbase/model.py
@@ -2,8 +2,8 @@
 
 from typing import Union
 
-from instancelib.environment.text import TextEnvironment
-from instancelib.instances.text import TextInstanceProvider
+from instancelib.environment.base import Environment
+from instancelib.instances.base import InstanceProvider
 from instancelib.machinelearning import (AbstractClassifier,
                                          SkLearnDataClassifier)
 
@@ -11,14 +11,14 @@ from .data import train_test_split
 
 
 def from_sklearn(model,
-                 environment: TextEnvironment,
-                 train: Union[float, int, TextInstanceProvider] = 0.7) -> AbstractClassifier:
+                 environment: Environment,
+                 train: Union[float, int, InstanceProvider] = 0.7) -> AbstractClassifier:
     """Wrap a scikit-learn model.
 
     Args:
         model ([type]): Model or pipeline from scikit-learn.
-        environment (TextEnvironment): Instancelib environment holding instances and corresponding ground-truth labels.
-        train (Union[float, int, TextInstanceProvider], optional): Training dataset (TextInstanceProvider), 
+        environment (Environment): Instancelib environment holding instances and corresponding ground-truth labels.
+        train (Union[float, int, InstanceProvider], optional): Training dataset (InstanceProvider), 
             fraction of training dataset size (float, [0, 1]) or instances in training set (int, > 0). Defaults to 0.7.
 
     Returns:
